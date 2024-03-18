0c9e535cab4cc39e6e7aaeed4179d6b9a69f1ada
Saikeertana Gubbala <gubbala3@msu.edu>
2024-03-18 19:18:30 -0400

    added chnages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("index.html");
     eleventyConfig.addPassthroughCopy("myjava.js");
+    eleventyConfig.addPassthroughCopy(".gitignore");
   };
--- myjava.js
+++ myjava.js
@@ -17,5 +17,4 @@ async function getBooks() {
   }
   
 }
-
 getBooks();
